e1334b3314b4996479046ec1e7eebcb1f4081f16
VissArea <112833032+VissArea@users.noreply.github.com>
2022-09-19 12:23:42 -0400

    Create TestGumMachine.java

---
+++ JavaApplications/GumMachine/TestGumMachine.java
@@ -0,0 +1,54 @@
+/***********************
+ *
+ *   TestGumMachine - demonstrates the GumMachine class
+ *
+   *   Author:  Jshua Visser
+ *   Date:     2020-Apr 1
+ *
+ ***********************/
+public class TestGumMachine {
+    public static void main(String[] args) {
+        System.out.println("\n--- Creating two machines, one with 20 gumballs, the other full:");
+        // write code to create two machines, one with 20, one full, and print them out
+
+
+        System.out.println("\n--- buying gum from each machine:");
+        // write code to buy gum once from each machine, print them out
+
+
+
+        System.out.println("\n--- buying gum from machine one 4 more times:");
+        // buy gum from first machine four times, print both out
+
+
+
+        System.out.println("\n--- Testing to see if gum machines are empty:");
+        // print the results of the isEmpty method for both machines
+
+
+
+        System.out.println("\n--- a loop to buy gum from second machine 90 times:");
+        // write a loop to buy gum from the full machine 90 times, print both out
+
+
+
+        System.out.println("\n--- Testing to see if gum machines are empty:");
+        // print the results of the isEmpty method for both machines
+
+
+
+        System.out.println("\n--- servicing both gum machines:");
+        // call the serviceMachine for both machines, print them out, and
+        // also print out how much money was retrieved from each machine.
+
+
+
+        System.out.println("\n--- Testing to see if gum machines are empty:");
+        // Once more, print the results of the isEmpty method for both machines
+
+
+
+
+        System.out.println();
+    } //main
+}
